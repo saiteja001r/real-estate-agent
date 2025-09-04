570d2563ae34d944f6170e041578d32ffad7b5b3
sai teja <saiteja001r@gmail.com>
2025-09-04 12:11:23 -0700

    added escalation check

---
+++ chains/escalation_check.py
@@ -0,0 +1,42 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GROQ_API_KEY")
+
+
+class EscalationCheck(BaseModel):
+    needs_escalation: bool = Field(
+        description="""whether the notice reqires escalation 
+        accourding to specified criteria"""
+    )
+
+
+escalation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Determine whether the following notice received
+            from a regulatory body requires immediate escalation.
+            Immediate escalation is required when {escalation_criteria}.add()
+
+            Here's the notice message:
+
+            {message}
+            """,
+        )
+    ]
+)
+
+escalation_check_model = init_chat_model(
+    "llama-3.1-8b-instant",
+    model_provider="groq", api_key=api_key)
+
+ESCALATION_CHECK_CHAIN = (
+    escalation_prompt
+    | escalation_check_model.with_structured_output(EscalationCheck)
+)
